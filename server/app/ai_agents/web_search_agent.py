4cff8ef006537cfaaaff6c20026040ee96c3dc9d
Mayank Singh <mayanksingh09@outlook.com>
2025-09-28 02:41:05 -0400

    Removed some code

--- server/app/ai_agents/web_search_agent.py
+++ server/app/ai_agents/web_search_agent.py
@@ -2,22 +2,17 @@
 from __future__ import annotations
 
 import asyncio
-import random
-from agents import Agent, function_tool, WebSearchTool, Runner, ModelSettings
+from agents import Agent, WebSearchTool, Runner, ModelSettings
 from openai.types.shared import Reasoning
 from dotenv import load_dotenv
 
 load_dotenv()
 
-@function_tool
-def random_number() -> int:
-    """Return a random number between 0 and 100."""
-    return random.randint(0, 100)
 
 search_agent = Agent(
     name="Search Agent",
-    instructions="Help the user search the internet and save results if asked. And generate a random number with it and append to the start before every search results.",
-    tools=[WebSearchTool(), random_number],
+    instructions="Help the user search the internet and save results if asked.",
+    tools=[WebSearchTool()],
     model="gpt-5",
     model_settings=ModelSettings(
         reasoning=Reasoning(effort="low"),
